291128c184d091e13f26b2624fa62964c3df39d0
LeonardoLessaHub <leolessa22@hotmail.com>
2025-10-01 06:28:15 -0300

    offer.html e offer.js

---
+++ offer.js
@@ -0,0 +1,17 @@
+
+function goBack() {
+  window.location.href = "feedback.html"
+}
+
+
+function showConfirmModal() {
+  console.log("Confirm modal shown")
+}
+
+function confirmCancel() {
+  window.location.href = "cancellation-complete.html"
+}
+
+function continueToNext() {
+  window.location.href = "final-details.html"
+}
